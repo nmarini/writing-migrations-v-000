3af86cc7739ad7273055ff66382c98179a56c188
Nicolas Marini <nmarini44@gmail.com>
2018-09-19 23:25:33 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::migrate[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migrate[5.1]
 
   def change
     change_column(artists, birthdate, datetime)
